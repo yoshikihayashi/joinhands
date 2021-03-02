a64bad7ea91d8ec91b2899c97b46e8d6340af66a
yoshikihayashi <yoshikixyz57@gmail.com>
2021-03-02 11:45:29 +0000

    マイグレーションファイル編集2

--- db/migrate/20210227073011_remove_company_id_from_reviews.rb
+++ db/migrate/20210227073011_remove_company_id_from_reviews.rb
@@ -1,5 +1,5 @@
 class RemoveCompanyIdFromReviews < ActiveRecord::Migration[5.2]
   def change
-    remove_column :reviews, :ompany_id, :integer
+    remove_column :reviews, :company_id, :integer
   end
 end
